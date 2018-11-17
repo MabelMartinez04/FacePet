7afc714d796189d9bc21e9d5ba98a081a60006fd
Mabel Martinez <mabbie.martinz@gmail.com>
2018-11-17 16:56:34 -0300

    Commit inicial

---
+++ app/src/main/java/py/com/prestosoftware/facepet/FacePetApplication.java
@@ -0,0 +1,12 @@
+package py.com.prestosoftware.facepet;
+
+import android.app.Application;
+
+public class FacePetApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
